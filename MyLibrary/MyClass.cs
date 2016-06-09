0491d5e6e3dd0897a8bc8879c7ed48c38bc272e3
Andy Alm <andy.alm@gettyimages.com>
2016-06-09 10:34:27 -0700

    netcoreapp1.0 referencing netstandard1.5 library doesn't work

---
+++ MyLibrary/MyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace MyLibrary
+{
+    public class MyClass
+    {
+        public static void Hello(TextWriter writer)
+        {
+            writer.WriteLine("Hello World!");
+        }
+    }
+}
